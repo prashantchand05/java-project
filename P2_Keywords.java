15df2e2250330596f7e227f140d75c25355e1187
prashantchand05 <91379348+prashantchand05@users.noreply.github.com>
2022-04-10 10:42:06 +0530

    Add files via upload

---
+++ P2_Keywords.java
@@ -0,0 +1,15 @@
+package Unit_01;
+
+public class P2_Keywords {
+	
+
+		public static void main(String[] args) {
+			int a=10;
+			/*
+			 * int double=20
+			 *  reason for this is we cannot use the double here as the token
+			 */
+			System.out.println(a);
+		}
+
+	}
