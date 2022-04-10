0f1f8a8b8dbbac81dc0ab16211a32a774becfa16
Prashant Chand <91379348+prashantchand05@users.noreply.github.com>
2022-04-10 11:26:32 +0530

    Add files via upload

---
+++ P3_VariablesAndDataTypes.java
@@ -0,0 +1,36 @@
+package Unit_01;
+
+public class P3_VariablesAndDataTypes {
+	static int q = 11 ;
+	int p =20 ;
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stu
+		
+		int a,b,c ;
+		float pi ;
+		double d ;
+		char e ;
+		
+		pi = 3.14f;
+		d = 20.22d;
+		e ='v' ;
+		a=10;
+		b =10; 
+		c =10 ;
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		
+		System.out.println(pi);
+		System.out.println(d);
+		System.out.println(e);
+	
+		
+		
+
+	}
+
+}
